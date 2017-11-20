eab4b3abb17adf73ff99c8b8536c399983f6a848
Daniel Diekmeier <fax@danieldiekmeier.de>
2017-11-20 20:16:05 +0100

    Vastly improve number display in recipes

--- lib/food_web/views/page_view.ex
+++ lib/food_web/views/page_view.ex
@@ -1,3 +1,18 @@
 defmodule FoodWeb.PageView do
   use FoodWeb, :view
+
+  def format_amount(amount) do
+    rounded = Float.round(amount)
+
+    case amount do
+      amount when amount == rounded ->
+        Kernel.trunc(amount)
+      0.5 ->
+        "½"
+      0.25 ->
+        "¼"
+      _ ->
+        amount
+    end
+  end
 end
