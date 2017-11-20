923b6a776274638d3bc6cd50452634131fb18be7
Daniel Diekmeier <fax@danieldiekmeier.de>
2017-11-20 19:32:00 +0100

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# These environment variables are sometimes needed by the running daemons
+export USER=danjel
+export HOME=/home/danjel
+
+# Include the user-specific profile
+source $HOME/.bash_profile
+
+export MIX_ENV=prod
+export PORT=65472
+export HOST="rezepte.danieldiekmeier.de"
+
+# Now let's go!
+cd /home/danjel/food
+git pull
+mix phx.digest.clean
+
+cd /home/danjel/food/assets
+./node_modules/.bin/brunch build -p
+
+cd /home/danjel/food
+mix ecto.reset
