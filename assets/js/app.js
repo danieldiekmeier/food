6716701fa6d641ff260c0cb533adc4e8c0b80494
Daniel Diekmeier <fax@danieldiekmeier.de>
2017-11-20 01:56:01 +0100

    Load GraphQL API in the frontend

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+/* globals Request, Headers, fetch */
+
 // Brunch automatically concatenates all files in your
 // watched paths. Those paths can be configured at
 // config.paths.watched in "brunch-config.js".
@@ -11,7 +13,7 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import 'phoenix_html'
 
 // Import local files
 //
@@ -19,3 +21,36 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+if (window.RECIPE_SLUG) {
+  console.log(window.RECIPE_SLUG)
+
+  const body = {
+    query: `{
+      recipe(slug: "pizza") {
+        name,
+        ingredients {
+          name
+          amount
+          unit
+        }
+      }
+    }`,
+    variables: null,
+    operationName: null
+  }
+
+  const req = new Request('/api', {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+
+  fetch(req).then((response) => {
+    return response.json()
+  }).then((body) => {
+    console.log(body.data.recipe)
+  })
+}
