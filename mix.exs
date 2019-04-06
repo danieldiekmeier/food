80e19bd0d387dba6c58897ce975b99786351f927
Daniel Diekmeier <fax@danieldiekmeier.de>
2019-04-06 22:34:38 +0200

    Update Deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Food.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"},
       {:comeonin, "~> 4.0"},
       {:argon2_elixir, "~> 1.2"},
       {:mix_test_watch, "~> 0.3", only: [:dev, :test], runtime: false},
